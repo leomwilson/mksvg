6dc1958523c3095f920370700460da1eecf6736b
Leo Wilson <leowilson.xlw123@gmail.com>
2019-06-02 22:06:51 -0700

    Write some code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs, process};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("Bad arguments");
+        process::exit(1);
+    }
+    let fname = &args[1];
+
+    let js = fs::read_to_string(fname)
+        .expect("Something went wrong reading the file");
+
+    let html = format!("<!DOCTYPE html><html><head><title>{}</title></head><body><div id=\"d\"></div><script src=\"https://cdnjs.cloudflare.com/ajax/libs/svg.js/3.0.12/svg.min.js\" integrity=\"sha256-IMCBafn5pmCrLYioAYBxLvBirr8BSVB3ocKfzKhHOz0=\" crossorigin=\"anonymous\"></script><script type=\"text/javascript\">var draw = SVG('d').size(\"100%\", \"100%\");{}</script></body></html>", fname, js);
+    println!("{}", html);
 }
